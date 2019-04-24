e9604359780d5b5da9c374aafa5807ede703d591
David <dhahn123@gmail.com>
2019-04-23 17:07:46 -0700

    Update App.js with DarkSky API, Socket IO Set Interval

--- app.js
+++ app.js
@@ -13,4 +13,21 @@ const server = http.createServer(app);
 // call to socketIo() to initialize a new instance by passing in the server object. By doing so we have wired up the ExpressJS server to Socket.IO.
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO"
+io.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+        () => getApiAndEmit(socket),
+        10000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+const getApiAndEmit = async socket => {
+    try {
+        const res = await axios.get(
+            "https://api.darksky.net/forecast/01f32ce226abfc6e048f54f3988b6456/43.7695,11.2558"
+        );
+        socket.emit("FromAPI", res.data.currently.temperature);
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+server.listen(port, () => console.log(`Listening on port ${port}`));
